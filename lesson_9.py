35794212cbbdc10aba07f4ccfdde6be7a017da5a
albanda89 <bandaraaruni899@gmail.com>
2023-06-04 07:40:49 +0200

    corrected code

---
+++ lesson_9.py
@@ -0,0 +1,28 @@
+# Task 1
+class Vehicle:
+    def __init__(self, max_speed, mileage, name):
+        self.max_speed = max_speed
+        self.mileage = mileage
+        self.name = name
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of {self.name} is {capacity}."
+
+car = Vehicle(200, 10000, "Tesla Model S")
+print(car.seating_capacity(5))
+
+class Bus(Vehicle):
+    color = "white"
+
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity)
+
+
+# Testing the program
+bus = Bus(120, 5000, "Volvo")
+print(f"Maximum Speed: {bus.max_speed}")
+print(f"Mileage: {bus.mileage}")
+print(f"Name: {bus.name}")
+print(f"Color: {bus.color}")
+print(bus.seating_capacity())
+print(bus.seating_capacity(60))
